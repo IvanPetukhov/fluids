00e9816e2450f991056ebcc8fb217a628d792b89
Ivan Petukhov <Ivan.Petukhov.CMC@gmail.com>
2016-12-26 02:30:19 +0300

    Update Main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -1,72 +1,103 @@
 module Main where
 
-import Lib
+import Computing
 import Types
 import Const
-import Computing
 import Graphics.Gloss.Interface.Pure.Game
 import Graphics.Gloss.Data.ViewPort
 
-import qualified Data.Foldable as Func
-
+-- Создание одной частицы
 initialPar :: Point -> Particle
-initialPar pos = (Particle pos  (0,0) (0,0) 0 0 (makeColor 0 0 1 1)) 
+initialPar pos = (Particle pos  (0,0) (0,0) 0 0 (makeColor 0 0 1 0.7)) 
 
+-- Создание всех частиц
 initialPars :: [Particle]
-initialPars = [(initialPar (x , y)) | x <- [-100,-80..(150)] , y <- [-100,-80..(150)] ]
+initialPars = [(initialPar (x , y)) | x <- posxCalc , y <- posyCalc]
 
+
+-- 
+posxCalc :: [Float]
+posxCalc = [(startX - pRadius * 2000 * numX / 2), (startX - pRadius * 2000 * numX / 2 + pRadius * 2000) .. (startX + (pRadius * 2000 * numX / 2 - pRadius * 2000))]
+
+
+posyCalc :: [Float]
+posyCalc = [(startY - pRadius * 2000 * numY / 2), (startY - pRadius * 2000 * numY / 2 + pRadius * 2000) .. (startY + (pRadius * 2000 * numY / 2 - pRadius * 2000))]
+
+--Создание границ
 initialBorders :: [Border]
-initialBorders = [(Border [(-7*k , -7*k) , (-7*k , 7*k)] (makeColor 0 0 0 1)) , (Border [(-7 *k, 7*k) , (7*k , 7*k)] (makeColor 0 0 0 1))
-                , (Border [(7*k , 7*k) , (7*k, -7*k)] (makeColor 0 0 0 1)) , (Border [(7*k , -7*k) , (-7*k , -7*k)] (makeColor 0 0 0 1))]
+initialBorders = [(Border [(-boxX, -boxY) , (-boxX, boxY)] (makeColor 0 0 0 1) (1, 0)) , (Border [(-boxX, boxY) , (boxX , boxY)] (makeColor 0 0 0 1) (0, -1))
+                , (Border [(boxX , boxY) , (boxX, -boxY)] (makeColor 0 0 0 1) (-1, 0)) , (Border [(boxX , -boxY) , (-boxX , -boxY)] (makeColor 0 0 0 1) (0, 1))]
+
+-- Создание картинки
 initialRoom :: Pic
 initialRoom = (Pic initialPars initialBorders)
 
+-- Отрисовка одной частицы
 drawPar :: Particle ->  Picture
-drawPar (Particle (x , y)  _ _ _ _ c) = translate x y (scale 50 50 (color c ( thickCircle (2 * 0.02) 0.25)))
+drawPar (Particle (x , y)  _ _ _ _ c) = translate x y (scale parScale parScale (color c ( thickCircle (pRadius * 1000 / 2) (pRadius * 1000))))
 
+--  Отрисовка прямоугольников
+drawPol1 :: Picture
+drawPol1 = (color (makeColor 1 0.8 0.8 1) (Polygon [( -winSizeX / 2, -winSizeY / 2) , ( -winSizeX / 2, winSizeY / 2) ,
+                                                    ( -boxX, winSizeY / 2) , ( -boxX, -winSizeY / 2)]))
+drawPol2 :: Picture
+drawPol2 = (color (makeColor 1 0.8 0.8 1) (Polygon [( -winSizeX / 2, -winSizeY / 2) , ( -winSizeX / 2, -boxY) ,
+                                                    ( winSizeX / 2, -boxY) , ( winSizeX / 2, -winSizeY / 2)]))
+drawPol3 :: Picture
+drawPol3 = (color (makeColor 1 0.8 0.8 1) (Polygon [( boxX, -winSizeY / 2) , ( winSizeX / 2, -winSizeY / 2) ,
+                                                    ( winSizeX / 2, winSizeY / 2) , ( boxX, winSizeY / 2)]))
+
+drawPol4 :: Picture
+drawPol4 = (color (makeColor 1 0.8 0.8 1) (Polygon [( winSizeX / 2, winSizeY / 2) , ( -winSizeX / 2, winSizeY / 2) ,
+                                                    ( -winSizeX / 2, boxY) , ( winSizeX / 2, boxY)]))
+drawPol5 :: Picture
+drawPol5 = (color (makeColor 0.5 0.8 0.8 1) (Polygon [( winSizeX / 2, winSizeY / 2) , ( -winSizeX / 2, winSizeY / 2) ,
+                                                    ( -winSizeX / 2, boxY)]))
+-- Отрисовка границ
 drawBorder :: Border -> Picture
-drawBorder (Border path c) = (color c (line path))
+drawBorder (Border path c _) = (color c (line path))
 
+-- Отрисовка картнки
 drawPic :: Pic ->Picture
-drawPic (Pic ps bs) = pictures ((Func.foldMap drawPar (reverse ps)) : (Func.foldMap drawBorder (reverse bs)) : [])
-
-
-changePosL1 :: Particle->Particle
-changePosL1 (Particle (x , y) a b c d e) = (Particle ((x-10) , y) a b c d e)
-
-changePosR1 :: Particle->Particle
-changePosR1 (Particle (x , y) a b c d e) = (Particle ((x+10) , y) a b c d e)
+drawPic (Pic ps bs) = pictures ((map drawPar ps) ++ (drawPol5 : [])++ (map drawBorder bs))
 
 handlePic :: Event -> Pic -> Pic
+handlePic (EventKey (MouseButton LeftButton) Down _ (x , y)) = addPart (x , y )
 handlePic (EventKey (SpecialKey KeyLeft) Down _ _)  = id
 handlePic (EventKey (SpecialKey KeyRight) Down _ _)  = id
 handlePic (EventMotion _)  = id
 handlePic (EventResize _)  = id
-handlePic (EventKey (Char _) _ _ _)  = id
+handlePic (EventKey (Char 'r') _ _ _)  = restart
+handlePic (EventKey (Char 'e') _ _ _)  = empty
 handlePic (EventKey (MouseButton _) _ _ _)  = id
 
-updatePar :: Float -> Particle -> [Border]->Particle
-updatePar time (Particle (x , y) a b c d e) bs = (Particle (newPos (x , y) time bs) a b c d e)
+-- Добавление новой частицы по нажатию левой клавиши мыши
+addPart :: Point -> Pic -> Pic
+addPart pos (Pic ps bs) = (Pic newPs bs)
+  where
+  	newPs = newPar : ps
+  	newPar = initialPar pos 
 
+-- Создание пустой картинки по клавише 'e'
+empty :: Pic -> Pic
+empty (Pic _ bs) = (Pic [] bs)
 
-newPos :: Point -> Float -> [Border] -> Point
-newPos (x,y) time bs = map (check (x,y) time) bs 
+-- Restart симуляции
+restart :: Pic -> Pic
+restart _ = initialRoom
 
-check :: Point -> Float -> Border -> Maybe Point
-check (x,y) time (Border [p1 p2] _) = intersectLineLine (x, y) (x+time, y) p1 p2
-
-get :: [Maybe]
 
 updatePic :: Float -> Pic -> Pic
-updatePic time (Pic ps bs) = (Pic (map (updatePar time bs) ps) bs)
+updatePic time (Pic ps bs) = (Pic (newPar ps bs (time * 10)) bs)
+                           
 
 
 main :: IO ()
 main = do
   play display bgColor fps initialRoom drawPic handlePic updatePic
   where
-    windowSize   = (820 , 820)
-    windowOffset = (250 , 250)
-    display = InWindow "SPH Fluid" windowSize windowOffset
-    bgColor = white
+    windowSize   = ((truncate winSizeX ) , (truncate winSizeY))
+    windowOffset = (250, 250)
+    display = InWindow "Fluids" windowSize windowOffset
+    bgColor = (makeColor 0.1 0.8 0.8 1)
     fps = 60
