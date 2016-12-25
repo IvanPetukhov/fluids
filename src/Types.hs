d56ace78ec12d9e5c5a8f8d649a0b266f0bb7a75
Ivan Petukhov <Ivan.Petukhov.CMC@gmail.com>
2016-12-26 02:29:37 +0300

    Update Types.hs

--- src/Types.hs
+++ src/Types.hs
@@ -2,20 +2,22 @@ module Types where
 import Graphics.Gloss.Interface.Pure.Game
 
 data Particle = Particle 
-					{
-						position:: Point,
-						velocity:: Point,
-						acc:: Point,
-						pressure:: Float,
-						density:: Float,
-						col:: Color
-				    }
+                    {
+                        position:: Point, -- Позиция
+                        velocity:: Point, -- Скорость
+                        pressure:: Float, -- Давление
+                        density:: Float,  -- Плотность
+                        col:: Color,      -- Цвет
+                        a :: Float,       -- Наличие доп ускорения
+                        t :: Float       -- тип жидкости
+                    }
 
 data Border = Border
-					{
-						bord :: Path,
-						colB :: Color,
-						normal :: Point
-					} 
+                    {
+                        bord :: Path,     -- 2 точки, по которым строить линию
+                        colB :: Color,    -- Цвет
+                        normal :: Point   -- Нормаль
+                    } 
 
-data Pic = Pic [Particle] [Border]
+data Pic = Pic [Particle] Float [Border] Float
+
